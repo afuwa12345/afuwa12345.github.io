07538992c236502aec63e01dfd98c61928f3d86f
afuwa12345 <afuwa344@gmail.com>
2026-04-24 22:06:29 +0900

    Add files via upload

--- chara_seiseki.js
+++ chara_seiseki.js
@@ -1,5 +1,5 @@
 // キャラ対決成績データ（自動更新）
-// 最終更新: 2026/04/20 10:28
+// 最終更新: 2026/04/20 11:58
 
 const CHARA_DATA = {
   "season": 2026,
@@ -7,37 +7,37 @@ const CHARA_DATA = {
   "characters": {
     "afuwa": {
       "name": "アフワ",
-      "img": "images/afuwa1.png",
+      "img": "images/afuwa.png",
       "color": "#4A90E2",
       "bet": "単勝500円＋複勝1500円"
     },
     "yancha": {
       "name": "やんちゃ",
-      "img": "images/afuwa5.png",
+      "img": "images/yanchaahuwa.png",
       "color": "#FF1A1A",
       "bet": "単勝2000円1点"
     },
     "buruma": {
       "name": "ブルマ",
-      "img": "images/buruma1.png",
+      "img": "images/buruma.png",
       "color": "#FF69B4",
       "bet": "ワイド3頭BOX＋単勝200円"
     },
     "chafuwa": {
       "name": "チャフワ",
-      "img": "images/chafuwa1.png",
+      "img": "images/chahuwa.png",
       "color": "#F5A623",
       "bet": "馬連3頭BOX＋単勝200円"
     },
     "kurofuwa": {
       "name": "クロフワ",
-      "img": "images/kurofuwa1.png",
+      "img": "images/kurohuwa.png",
       "color": "#9B59B6",
       "bet": "三連複5頭BOX"
     },
     "ginma": {
       "name": "ギンマ",
-      "img": "images/ginma1.png",
+      "img": "images/ginma.png",
       "color": "#2ECC71",
       "bet": "馬単BOX＋三連単BOX＋単勝200円"
     }
@@ -49,42 +49,74 @@ const CHARA_DATA = {
       "results": {
         "afuwa": {
           "race": "中山 5R",
+          "date": "04/19",
           "horse": "マーゴットブロー",
+          "aite": [],
+          "bet": "単勝500円＋複勝1500円",
           "result": "○",
           "payout": 5300,
           "chaku": 1
         },
         "yancha": {
           "race": "福島 1R",
+          "date": "04/19",
           "horse": "スプリングドリーム",
+          "aite": [],
+          "bet": "単勝2000円1点",
           "result": "×",
           "payout": 0,
           "chaku": 9
         },
         "buruma": {
           "race": "阪神 2R",
+          "date": "04/19",
           "horse": "ヤマヤロード",
+          "aite": [
+            "ルクスキャンディ",
+            "カトマンズゴールド"
+          ],
+          "bet": "ワイド3頭BOX＋単勝200円",
           "result": "×",
           "payout": 0,
           "chaku": 12
         },
         "chafuwa": {
           "race": "中山 1R",
+          "date": "04/18",
           "horse": "アドミ",
+          "aite": [
+            "セントリアン",
+            "クアロアランチ"
+          ],
+          "bet": "馬連3頭BOX＋単勝200円",
           "result": "×",
           "payout": 0,
           "chaku": 3
         },
         "kurofuwa": {
           "race": "阪神 8R",
+          "date": "04/19",
           "horse": "レヴァンテシチー",
+          "aite": [
+            "オリジナルファイン",
+            "メイショウオグマ",
+            "レッドイステル",
+            "メイショウカスガイ"
+          ],
+          "bet": "三連複5頭BOX",
           "result": "×",
           "payout": 0,
           "chaku": 1
         },
         "ginma": {
           "race": "中山 11R",
+          "date": "04/19",
           "horse": "バステール",
+          "aite": [
+            "アドマイヤクワッズ",
+            "ゾロアストロ"
+          ],
+          "bet": "馬単BOX＋三連単BOX＋単勝200円",
           "result": "×",
           "payout": 0,
           "chaku": 11
